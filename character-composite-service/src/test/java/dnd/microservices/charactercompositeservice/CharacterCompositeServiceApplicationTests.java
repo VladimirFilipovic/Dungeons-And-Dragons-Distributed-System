8fde851ad710258c29084e1eacbbbc519e88beb4
L-48\Vladimir <vladimir.filipovic@tiacgroup.com>
2023-01-21 15:16:35 +0100

    creates containers for each service

--- character-composite-service/src/test/java/dnd/microservices/charactercompositeservice/CharacterCompositeServiceApplicationTests.java
+++ character-composite-service/src/test/java/dnd/microservices/charactercompositeservice/CharacterCompositeServiceApplicationTests.java
@@ -1,10 +1,14 @@
 package dnd.microservices.charactercompositeservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest
 class CharacterCompositeServiceApplicationTests {
+	@Autowired
+	private WebTestClient client;
 
 	@Test
 	void contextLoads() {
