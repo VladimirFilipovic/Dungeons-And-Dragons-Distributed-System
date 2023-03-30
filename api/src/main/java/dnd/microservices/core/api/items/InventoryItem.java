17e6c92b3f720cadc65a5258fc01abbda261a50a
L-48\Vladimir <vladimir.filipovic@tiacgroup.com>
2023-03-30 23:34:07 +0200

    Refined inventory service and types

--- api/src/main/java/dnd/microservices/core/api/items/InventoryItem.java
+++ api/src/main/java/dnd/microservices/core/api/items/InventoryItem.java
@@ -1,6 +1,6 @@
 package dnd.microservices.core.api.items;
 
-public class InventoryItemDto {
+public class InventoryItem {
 
      public String itemName;
    
@@ -8,10 +8,10 @@ public class InventoryItemDto {
 
     public ModificationType modificationType;
    
-    public InventoryItemDto() {
+    public InventoryItem() {
     }
 
-    public InventoryItemDto(String itemName, int amount, ModificationType modificationType) {
+    public InventoryItem(String itemName, int amount, ModificationType modificationType) {
         this.itemName = itemName;
         this.amount = amount;
         this.modificationType = modificationType;
---
+++ api/src/main/java/dnd/microservices/core/api/items/InventoryService.java
@@ -0,0 +1,23 @@
+package dnd.microservices.core.api.items;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public interface InventoryService {
+    
+    @GetMapping(
+            value    = "/character-inventory/{character-name}",
+            produces = "application/json"
+        )
+    List<InventoryItem> getCharacterInventory(@PathVariable String characterName);
+   
+    @PostMapping(
+        value = "/character-inventory/{character-name}", 
+        consumes = "application/json",
+        produces = "application/json"
+    )
+    void modifyCharacterInventory(@PathVariable String characterName, InventoryItem body);
+}
