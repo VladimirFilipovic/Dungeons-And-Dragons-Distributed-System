a86c742a1d0d55404be31257723cb4c90779a9e3
vladimirf <vlada.19982309@gmail.com>
2023-06-01 20:33:28 +0200

    fix user tests

--- character-service/src/main/java/dnd/microservices/core/characterservice/services/CharacterMapper.java
+++ character-service/src/main/java/dnd/microservices/core/characterservice/services/CharacterMapper.java
@@ -1,8 +1,11 @@
 package dnd.microservices.core.characterservice.services;
 
+import org.mapstruct.Mapper;
+
 import dnd.microservices.core.api.character.Character;
 import dnd.microservices.core.characterservice.persistance.CharacterEntity;
 
+@Mapper(componentModel = "spring")
 public class CharacterMapper {
 
     public CharacterEntity apiToEntity(Character body) {
